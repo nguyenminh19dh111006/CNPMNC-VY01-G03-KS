ffe91c89e1c3c35d3eb6616866107e1e7ebda83c
nguyenminh19dh111006 <102563489+nguyenminh19dh111006@users.noreply.github.com>
2022-05-15 13:46:41 +0700

    ver 2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Login from "./Components/Login";
