dcf450a74e460f6d085740fc3afb97cc23d9587f
nguyenminh19dh111006 <102563489+nguyenminh19dh111006@users.noreply.github.com>
2022-06-07 19:06:50 +0700

    ver final

--- src/Components/Signup/SignupForm.js
+++ src/Components/Signup/SignupForm.js
@@ -1,7 +1,49 @@
-import React, {} from "react";
+import React, {useState} from "react";
+import { Navigate } from "react";
 import { Link } from "react-router-dom";
+import Axios from "axios";
 
 function SignupForm() {
+    const [IdPartner, setIdPartner] = useState('');
+    const [TenKhachSan, setTenKhachSan] = useState('');
+    const [TenPhong, setTenPhong] = useState('');
+    const [ThayDoiChinhSach, setThayDoiChinhSach] = useState();
+    const [TenTaiKhoan, setTenTaiKhoan] = useState('');
+    const [MatKhau, setMatKhau] = useState();
+    const [redirectHome,setRedirectHome] = useState(false);
+    const registerPartner = `http://157.245.207.242:8090/api/partner`;
+
+if(redirectHome) {
+    return <Link to={'/partner'} replace/>
+}
+
+    const changeIdPartner = e => setIdPartner(e.target.value);
+    const changeTenKhachSan = e => setTenKhachSan(e.target.value);
+    const changeTenPhong = e => setTenPhong(e.target.value);
+    const changeThayDoiChinhSach = e => setThayDoiChinhSach(e.target.value);
+    const changeTenTaiKhoan = e => setTenTaiKhoan(e.target.value);
+    const changeMatKhau = e => setMatKhau(e.target.value);
+
+    const handleRegister = (e) =>{
+
+    Axios.post(registerPartner, {
+        IdPartner:`${IdPartner}`,
+        TenKhachSan: `${TenKhachSan}`,
+        TenPhong: `${TenPhong}`,
+        ThayDoiChinhSach: `${ThayDoiChinhSach}`,
+        TenTaiKhoan: `${TenTaiKhoan}` ,
+        MatKhau: `${MatKhau}` ,
+      }).then((res) => {
+        localStorage.setItem('USER_LOGIN_INFORMATION', JSON.stringify(res.data));
+        if(res.status == 201) {
+            setRedirectHome(true)
+        }
+     })
+     .catch((error) => {
+     console.error(error)
+    });
+      e.preventDefault();
+    }
     return (
         <div className="bg-gray-100  flex flex-col justify-center items-center py-32">
             <h1 className="text-3xl font-semibold text-center">
@@ -11,54 +53,82 @@ function SignupForm() {
             <form
                 action=""
                 className="w-11/12 sm:w-10/12 lg:w-6/12 xl:w-4/12 bg-gray-200 p-10 rounded-sm  mt-10 border shadow-lg">
-                <label htmlFor="first_name" className="block">
-                    First Name:{" "}
+                <label htmlFor="IdPartner" className="block">
+                    Id:{" "}
                 </label>
                 <input
-                    id="first_name"
+                    onChange={changeIdPartner}
+                    placeholder="Nhập Id"
                     type="text"
-                    name="first_name"
+                    name="IdPartner"
                     className="p-2 w-full border border-gray-400 focus:outline-none focus:border-black"
                     aria-required="true"/>
 
-                <label htmlFor="last_name" className="block mt-5">
-                    Last Name:{" "}
+                <label htmlFor="TenKhachSan" className="block mt-5">
+                    Tên khách sạn:{" "}
                 </label>
                 <input
-                    id="last_name"
+                    onChange={changeTenKhachSan}
+                    placeholder="Nhập tên khách sạn"
                     type="text"
-                    name="last_name"
+                    name="TenKhachSan"
                     className="p-2 w-full border border-gray-400 focus:outline-none focus:border-black"
                     aria-required="true"/>
 
-                <label htmlFor="email" className="block mt-5">
-                    Email:{" "}
+                <label htmlFor="TenPhong" className="block mt-5">
+                    Tên phòng:{" "}
                 </label>
                 <input
-                    id="email"
-                    type="email"
-                    name="email"
+
+                    onChange={changeTenPhong}
+                    placeholder="Nhập tên phòng"
+                    type="text"
+                    name="TenPhong"
                     className="p-2 w-full border border-gray-400 focus:outline-none focus:border-black"
                     aria-required="true"/>
 
-                <label htmlFor="password" className="block mt-5">
+                <label htmlFor="ThayDoiChinhSach" className="block mt-5">
+                    Chính sách(nếu có):{" "}
+                </label>
+                <input
+                    onChange={changeThayDoiChinhSach}
+                    placeholder="Nhập chính sách"
+                    type="text"
+                    name="ThayDoiChinhSach"
+                    className="p-2 w-full border border-gray-400 focus:outline-none focus:border-black"
+                    aria-required="true"/>
+
+                <label htmlFor="TenTaiKhoan" className="block mt-5">
+                    Username:{" "}
+                </label>
+                <input
+                    onChange={changeTenTaiKhoan}
+                    placeholder="Nhập tên tài khoản"
+                    type="text"
+                    name="TenTaiKhoan"
+                    className="p-2 w-full border border-gray-400 focus:outline-none focus:border-black"
+                    aria-required="true"/>
+
+                <label htmlFor="MatKhau" className="block mt-5">
                     Password:{" "}
                 </label>
                 <input
-                    id="password"
+                    onChange={changeMatKhau}
+                    placeholder="Nhập mật khẩu"
                     type="password"
-                    name="password"
+                    name="MatKhau"
                     className="p-2 w-full border border-gray-400 focus:outline-none focus:border-black"
                     aria-required="true"/>
 
                 <div className="mt-5 flex flex-col md:flex-row justify-between items-center">
-                    <button
+                    <input
+                        value="ĐĂNG KÝ"
+                        name="submit"
                         className="text-center bg-blue-600 text-white hover:bg-blue-700 uppercase text-sm px-12 py-4 shadow
                     hover:shadow-lg"
                         type="submit"
-                        aria-label="sign up">
-                        Tạo tài khoản
-                    </button>
+                        onClick={handleRegister}/>
+                       
 
                     <Link to="/login" className="text-blue-800 mt-5 md:mt-0 hover:text-blue-600">
                         Đã có tài khoản ?
