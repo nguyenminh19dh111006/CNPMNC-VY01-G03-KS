6ff684a5cfcbb95df51ed846e35d0f032caddcea
nguyenminh19dh111006 <102563489+nguyenminh19dh111006@users.noreply.github.com>
2022-06-12 10:08:20 +0700

    ver 2

--- src/Components/Partner/PostForm.js
+++ src/Components/Partner/PostForm.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
+import { Link } from "react-router-dom";
 import Axios from 'axios';
 
 function closeForm() {
